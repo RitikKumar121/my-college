23aa577c42029b3c81e93dd2909396d7b177ea5b
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2021-06-30 21:13:26 +0530

    commit

--- clg/clg/clg/settings.py
+++ clg/clg/clg/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-#STATICFILES_DIRS = [
- #   STATIC_DIR,
-#]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Default primary key field type
@@ -140,6 +140,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL='/static/'
+
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
