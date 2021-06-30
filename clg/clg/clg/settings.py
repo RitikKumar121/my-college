04c8519dbf7a2471fb2cf5afce9b73e4cae4db4b
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2021-06-30 20:48:21 +0530

    commit

--- clg/clg/clg/settings.py
+++ clg/clg/clg/settings.py
@@ -142,4 +142,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL='/static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
-STATIC_ROOT=os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
