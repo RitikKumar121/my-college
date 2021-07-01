7107b687af9944bd225726ee32087a3fe7f102e8
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2021-07-01 12:40:55 +0530

    commit

--- clg/clg/clg/settings.py
+++ clg/clg/clg/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-qldedgsb)!z598-u*(%w5-$#k1uucy2%z8@7fqv3b&xxm8478%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['collegelearning.herokuapp.com']
 
